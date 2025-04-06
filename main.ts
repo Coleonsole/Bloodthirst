3f3d121352139f5364955cad9ca762f8b0f2e75d
Coleonsole <Mightymiloth@gmail.com>
2025-04-06 10:50:23 +0100

    Day 2 added and minor bug fixed

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+namespace SpriteKind {
+    export const Interaction = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -19,9 +22,10 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
         `, mySprite, Render.getAttribute(Render.attribute.dirX) * 50, Render.getAttribute(Render.attribute.dirY) * 50)
 })
 function game2 () {
-    game.splash("Bloodthirst")
-    game.splash("A Coleonsole original")
+    game.splash("Day 1")
+    mySprite = Render.getRenderSpriteVariable()
     tiles.setCurrentTilemap(tilemap`level2`)
+    tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
     villain = sprites.create(img`
         . . . . . . 5 . 5 . . . . . . . 
         . . . . . f 5 5 5 f f . . . . . 
@@ -40,8 +44,6 @@ function game2 () {
         . . . f f f f f f f f f 2 . . . 
         . . . . . f f . . f f . 2 . . . 
         `, SpriteKind.Enemy)
-    Render.move(villain, 30)
-    villain.follow(Render.getRenderSpriteInstance(), 30)
     tiles.placeOnRandomTile(villain, sprites.dungeon.floorDarkDiamond)
     scene.setBackgroundImage(img`
         ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
@@ -165,8 +167,6 @@ function game2 () {
         cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
         cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
         `)
-    tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
-    mySprite = Render.getRenderSpriteVariable()
     Render.moveWithController(2, 3, 1)
     Render.move(mySprite, 120)
     crosshair = sprites.create(img`
@@ -188,12 +188,22 @@ function game2 () {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Player)
     crosshair.setFlag(SpriteFlag.RelativeToCamera, true)
+    Render.move(villain, 30)
+    villain.follow(Render.getRenderSpriteInstance(), 30)
+}
+function Day_2 () {
+    game.splash("The doll is dead...")
+    game.splash("You'll never be alone again")
+    game.splash("Day 2")
+    tiles.loadMap(tiles.createMap(tilemap`level6`))
 }
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprites.destroy(otherSprite, effects.fire, 500)
+    Day_2()
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     music.play(music.createSong(hex`0078000408020400001c00010a006400f4016400000400000000000000000000000000050000041100000004000c191b1d1e2022242527292a2c01001c000f05001202c102c201000405002800000064002800031400060200041100000004000c191b1d1e2022242527292a2c02001c000c960064006d019001000478002c010000640032000000000a0600051100000004000c191b1d1e2022242527292a2c03001c0001dc00690000045e01000400000000000000000000056400010400030d000000040008191b1e242527292c`), music.PlaybackMode.InBackground)
+    game.setGameOverMessage(false, "you were caught")
     game.setGameOverEffect(false, effects.splatter)
     game.gameOver(false)
     game.reset()
@@ -202,6 +212,9 @@ let crosshair: Sprite = null
 let villain: Sprite = null
 let mySprite: Sprite = null
 let projectile: Sprite = null
+game.splash("Bloodthirst")
+game.splash("Press (a) to begin")
+pauseUntil(() => controller.A.isPressed())
 game2()
 forever(function () {
     music.play(music.createSong(hex`003c000408050203001c0001dc00690000045e0100040000000000000000000005640001040003060000000400011b09010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c8009c000400050001000c000d0001001000110001001800190001001c001d0001002400250001002800290001003000310001003400350001003c003d0001004000410001004800490001004c004d0001005400550001005800590001006000610001006400650001006c006d0001007000710001007800790001007c007d0001008400850001008800890001009000910001009400950001009c009d000100`), music.PlaybackMode.UntilDone)
